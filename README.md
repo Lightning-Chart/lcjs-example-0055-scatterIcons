4ce1077ec66da0bb3df1a62441e3d670ec8af07c
Niilo Keinänen <niilo.keinanen@lightningchart.com>
2024-05-07 14:00:50 +0300

    Initial

---
+++ README.md
@@ -0,0 +1,62 @@
+# JavaScript Annotation Chart
+
+![JavaScript Annotation Chart](scatterIcons-darkGold.png)
+
+This demo application belongs to the set of examples for LightningChart JS, data visualization library for JavaScript.
+
+LightningChart JS is entirely GPU accelerated and performance optimized charting library for presenting massive amounts of data. It offers an easy way of creating sophisticated and interactive charts and adding them to your website or web application.
+
+The demo can be used as an example or a seed project. Local execution requires the following steps:
+
+-   Make sure that relevant version of [Node.js](https://nodejs.org/en/download/) is installed
+-   Open the project folder in a terminal:
+
+          npm install              # fetches dependencies
+          npm start                # builds an application and starts the development server
+
+-   The application is available at _http://localhost:8080_ in your browser, webpack-dev-server provides hot reload functionality.
+
+
+## Description
+
+Example showcasing use of custom Icons as shape of a Point series.
+In this example, a .png image asset of a flag is loaded as a custom icon, and used as the shape of a point series.
+This point series is then used to display flag annotations bound to a timestamp that can be dragged and dropped by the user. The flags are also available as different colors.
+
+As such, this also showcases a simple example of chart as a drag & drop target.
+
+See also, [displaying custom images in scatter chart](https://lightningchart.com/js-charts/interactive-examples/examples/lcjs-example-0054-scatterImageFill.html).
+
+
+## API Links
+
+* [Lightning chart]
+* [Chart XY]
+* [Axis]
+
+
+## Support
+
+If you notice an error in the example code, please open an issue on [GitHub][0] repository of the entire example.
+
+Official [API documentation][1] can be found on [LightningChart][2] website.
+
+If the docs and other materials do not solve your problem as well as implementation help is needed, ask on [StackOverflow][3] (tagged lightningchart).
+
+If you think you found a bug in the LightningChart JavaScript library, please contact sales@lightningchart.com.
+
+Direct developer email support can be purchased through a [Support Plan][4] or by contacting sales@lightningchart.com.
+
+[0]: https://github.com/Arction/
+[1]: https://lightningchart.com/lightningchart-js-api-documentation/
+[2]: https://lightningchart.com
+[3]: https://stackoverflow.com/questions/tagged/lightningchart
+[4]: https://lightningchart.com/support-services/
+
+© LightningChart Ltd 2009-2022. All rights reserved.
+
+
+[Lightning chart]: https://lightningchart.com/js-charts/api-documentation/v5.2.0/functions/lightningChart-1.html
+[Chart XY]: https://lightningchart.com/js-charts/api-documentation/v5.2.0/classes/ChartXY.html
+[Axis]: https://lightningchart.com/js-charts/api-documentation/v5.2.0/classes/Axis.html
+
---
+++ src/index.js
@@ -0,0 +1,105 @@
+const lcjs = require('@arction/lcjs')
+const { lightningChart, Themes, emptyFill, AxisTickStrategies, AxisScrollStrategies, emptyLine, IndividualPointFill, ColorHEX } = lcjs
+
+const imgFlag = new Image()
+imgFlag.crossOrigin = ''
+imgFlag.src = document.head.baseURI + 'examples/assets/0055/flag.png'
+
+let channels = new Array(3).fill(0).map((_, i) => ({ label: `Channel ${i + 1}` }))
+const lc = lightningChart({
+            resourcesBaseUrl: new URL(document.head.baseURI).origin + new URL(document.head.baseURI).pathname + 'resources/',
+        })
+const chart = lc
+    .ChartXY({
+        theme: Themes[new URLSearchParams(window.location.search).get('theme') || 'darkGold'] || undefined,
+    })
+    .setTitle('Drag different colored annotations from left')
+chart.getDefaultAxisY().dispose()
+channels = channels.map((ch, i) => {
+    const iStack = channels.length - (i + 1)
+    const axisY = chart
+        .addAxisY({ iStack })
+        .setDefaultInterval({ start: 0, end: 1 })
+        .setMargins(iStack > 0 ? 15 : 0, iStack < channels.length - 1 ? 15 : 0)
+        .setMouseInteractions(false)
+        .setChartInteractions(false)
+    const series = chart
+        .addPointLineAreaSeries({ axisY, dataPattern: 'ProgressiveX' })
+        .setAreaFillStyle(emptyFill)
+        .setMaxSampleCount(100_000)
+    return { axisY, series }
+})
+const timeAxis = chart
+    .getDefaultAxisX()
+    .setTickStrategy(AxisTickStrategies.Time)
+    .setScrollStrategy(AxisScrollStrategies.progressive)
+    .setDefaultInterval((state) => ({
+        end: state.dataMax ?? 0,
+        start: (state.dataMax ?? 0) - 15_000,
+        stopAxisAfter: false,
+    }))
+
+setInterval(() => {
+    const x = performance.now()
+    channels.forEach((ch) => {
+        const y = Math.random()
+        ch.series.appendSample({ x, y })
+    })
+}, 1000 / 60)
+
+// Drag & drop annotations
+const annotationMenu = document.createElement('div')
+chart.engine.container.append(annotationMenu)
+annotationMenu.style.position = 'absolute'
+annotationMenu.style.display = 'flex'
+annotationMenu.style.flexDirection = 'row'
+annotationMenu.style.margin = '10px'
+;[
+    { color: '#ff0000', label: 'Red' },
+    { color: '#00ff00', label: 'Green' },
+    { color: '#0000ff', label: 'Blue' },
+].forEach((item) => {
+    const option = document.createElement('span')
+    annotationMenu.append(option)
+    option.style.height = option.style.width = '20px'
+    option.style.fontFamily = 'Segoe UI'
+    option.style.backgroundColor = item.color
+    option.style.marginRight = '10px'
+    option.style.border = `solid ${chart.getTheme().isDark ? 'white' : 'black'} 1px`
+    option.style.borderRadius = '5px'
+    option.style.cursor = 'pointer'
+    option.draggable = true
+    option.ondragstart = (event) => {
+        event.dataTransfer.setData('text', JSON.stringify(item))
+    }
+})
+const uiAxisY = chart
+    .addAxisY({ opposite: true })
+    .setTickStrategy(AxisTickStrategies.Empty)
+    .setStrokeStyle(emptyLine)
+    .setMouseInteractions(false)
+    .setInterval({ start: 0, end: 1 })
+const pointSeries = chart
+    .addPointLineAreaSeries({
+        axisY: uiAxisY,
+        dataPattern: null,
+        colors: true,
+    })
+    .setAreaFillStyle(emptyFill)
+    .setStrokeStyle(emptyLine)
+    .setPointFillStyle(new IndividualPointFill())
+    .setPointShape(chart.engine.addCustomIcon(imgFlag))
+    .setPointSize(0.2)
+    .setPointAlignment({ x: -1, y: 1 })
+    .setAutoScrollingEnabled(false)
+    .setCursorEnabled(false)
+chart.engine.container.ondragover = (event) => event.preventDefault()
+chart.engine.container.ondrop = (event) => {
+    const item = JSON.parse(event.dataTransfer.getData('text'))
+    const x = chart.translateCoordinate(event, chart.coordsAxis).x
+    // Annotation is built from 2 components:
+    // 1. ConstantLine (vertical line created on X axis)
+    // 2. A single sample in Point series with Icon shape and individual sample colors.
+    timeAxis.addConstantLine(false).setValue(x)
+    pointSeries.appendSample({ x, y: 1, color: ColorHEX(item.color) })
+}
